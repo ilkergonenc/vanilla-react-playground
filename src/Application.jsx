ead800c318c7edc498de399f9902f92c882bb795
ig <ilkergonenc@gmail.com>
2022-08-03 20:45:39 +0300

    app to application

---
+++ src/Application.jsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { HelmetProvider } from 'react-helmet-async';
+
+import Seo from './components/Seo';
+import Home from './routes/Home';
+
+function Application() {
+  return (
+    <HelmetProvider>
+      <Seo title="My Vanilla React App" description="My Vanilla React Application based on react-script & react-router." />
+      <Home />
+    </HelmetProvider>
+  );
+}
+
+export default App;
