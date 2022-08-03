a1b39affb7759e700b9bf3295bdce4fb4323b2e3
ig <ilkergonenc@gmail.com>
2022-08-03 21:03:29 +0300

    import

--- src/Application.jsx
+++ src/Application.jsx
@@ -1,27 +1,26 @@
 import * as React from 'react';
 import { HelmetProvider } from 'react-helmet-async';
-import loadable from '@loadable/component';
-import pMinDelay from 'p-min-delay';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Seo from './components/Seo';
-
-function Loading() {
-  return (
-    <div className="h-[100vh] flex justify-center items-center">
-      <p>Loading...</p>
-    </div>
-  );
-}
-
-const Home = loadable(() => pMinDelay(import('./routes/Home'), 200), {
-  fallback: <Loading />,
-});
+import {
+  Home,
+  NotFound,
+} from './routes/@loadables';
 
 function Application() {
   return (
     <HelmetProvider>
-      <Seo title="My Vanilla React App" description="My Vanilla React Application based on react-script & react-router." />
-      <Home />
+      <Seo
+        title="My Vanilla React App"
+        description="My Vanilla React Application based on react-script & react-router."
+      />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </HelmetProvider>
   );
 }
