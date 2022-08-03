742662a84848a3429709c8ad27dce149100a5a1d
ig <ilkergonenc@gmail.com>
2022-08-03 21:51:46 +0300

    responsive fix

--- src/routes/NotFound.jsx
+++ src/routes/NotFound.jsx
@@ -5,7 +5,7 @@ function NotFound({
 }) {
   return (
     <section className="h-[80vh] flex flex-col justify-center items-center px-8 space-y-4">
-      <h1 className="text-[16rem]">{type}</h1>
+      <h1 className="text-[12rem] lg:text-[16rem]">{type}</h1>
       <h5 className="text-3xl">{title}</h5>
       <p className="text-xl">{descrition}</p>
       {actions.map((action) => <a href={action.link}>{action.title}</a>)}
