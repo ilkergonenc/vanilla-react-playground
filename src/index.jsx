1d8320a261081999cc88e8e3998ff4b4d26ae866
ig <ilkergonenc@gmail.com>
2022-08-03 20:46:29 +0300

    update && fix

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,13 @@ import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './tailwind.css';
-import App from './App';
+import Application from './Application';
 import reportWebVitals from './utils/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Application />
   </React.StrictMode>,
 );
 
