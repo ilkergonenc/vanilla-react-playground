cc8e7ba67d1b42d491eebd09d4012addc28f5b31
ig <ilkergonenc@gmail.com>
2022-08-03 21:18:45 +0300

    static data via js object

---
+++ src/app.static.js
@@ -0,0 +1,20 @@
+module.exports = {
+  name: 'Veact',
+  ver: '0.0.1',
+  meta: {
+    title: 'Vanilla React Application',
+    description: '',
+  },
+  navs: {
+    main: [
+      {
+        title: 'Home',
+        link: '/',
+      },
+      {
+        title: '404',
+        link: '/404',
+      },
+    ],
+  },
+};
